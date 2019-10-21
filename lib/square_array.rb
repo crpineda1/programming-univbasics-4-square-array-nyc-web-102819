902e04a57fec671384bfcf5dd9eb2532ec314690
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-21 18:36:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(myarray)
   array1 = []
   while myarray.length != null do
     #array1[count] = myarray[count]**2
-    square_array(array)
+    square_array(myarray)
     #count += 1
   end
   p array1
